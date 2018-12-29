0b83ac4938e1537377f885cca4b9163dfb39efe8
horpto <__singleton__@hackerdom.ru>
2018-12-29 16:20:30 +0500

    add template for iteration loop

--- tetris/main.py
+++ tetris/main.py
@@ -1,22 +1,59 @@
 from tkinter import *
 from tkinter.ttk import *
 import tkinter as tk
+from enum import Enum
+import random
+import time
 
-class CellColor:
+
+class CellColor(Enum):
     EMPTY = ""
     RED = "red"
     GREEN = "green"
     BLUE = "blue"
 
+    @classmethod
+    def choose_one(cls):
+        return random.choice([c for c in cls if c != cls.EMPTY])
+
+class Figure:
+    cells = ()
+
+    def can_turn(self, fields):
+        pass
+
+    def turn(self, fields):
+        pass
+
+    def set_fields(self, fields):
+        for y, x, color in self.cells:
+            fields[y][x] = color
+
+    def reset_fields(self, fields):
+        for y, x, color in self.cells:
+            fields[y][x] = CellColor.EMPTY
+
+
+class SquareFigure(Figure):
+    def __init__(self, i, j):
+        self.y, self.x = i, j
+
+        self.cells = [
+            (i, j, CellColor.choose_one()), (i, j+1, CellColor.choose_one()),
+            (i+1, j, CellColor.choose_one()), (i+1, j+1, CellColor.choose_one())
+        ]
+
+
 class Tetris:
     cell_size = 25
 
-    def __init__(self, root, width, height):
-        self.root = root              
+    def __init__(self, root, width=10, height=21, speed=0.3):
+        self.root = root
         self.width = width
         self.height = height
+        self.speed = speed # speed of figure falling (in seconds)
 
-        self.field = [[CellColor.RED]*width  for i in range(height)]
+        self.field = [[CellColor.EMPTY]*width  for i in range(height)]
 
         self.canvas_width = width * self.cell_size
         self.canvas_height = height * self.cell_size,
@@ -28,6 +65,8 @@ class Tetris:
         self.score = 0
         self.label = Label(text="Score:")
         self.score_text = Label(text=str(self.score))
+
+        self.prev_timer = 0
 
     def pack(self):
         self.canvas.pack(side=LEFT)
@@ -44,22 +83,34 @@ class Tetris:
         for row in self.field:
             offset = 0
             for color in row:
-                canvas.create_rectangle(offset, y_offset, offset + self.cell_size, y_offset + self.cell_size, fill=color)
+                canvas.create_rectangle(offset, y_offset, offset + self.cell_size, y_offset + self.cell_size, fill=color.value)
                 offset += self.cell_size
             y_offset += self.cell_size
         canvas.create_line
+
+    def configure(self):
+        self.pack()
+        self.draw_field()
+        self._start_loop()
+
+    def _start_loop(self):
+        if time.monotonic() - self.prev_timer >= self.speed:
+            self._iteration()
+            self.prev_timer = time.monotonic()
+        self.root.after(100, self._start_loop)
+
+    def _iteration(self):
+        print('iter', time.monotonic())
         
-        
+
+
 def main():
     root = Tk()
     root.title("TetrisEd")
 
-    game = Tetris(root, width=10, height=21)
-    game.pack()
+    game = Tetris(root)
+    game.configure()
 
-    game.draw_field()
-
-    print('mainloop')
     root.mainloop()
 
 if __name__ == "__main__":
